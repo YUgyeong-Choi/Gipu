d54585ca4c2e0ad07af577738d7718d7f4531461
yuooji <103304415+yuooji@users.noreply.github.com>
2023-09-19 15:48:00 +0900

    9.19

--- app/src/main/java/com/example/gipu_android/WritingActivity.kt
+++ app/src/main/java/com/example/gipu_android/WritingActivity.kt
@@ -1,6 +1,11 @@
 package com.example.gipu_android
 
+import android.content.Intent
 import android.os.Bundle
+import android.text.TextWatcher
+import android.text.Editable
+import android.widget.PopupMenu
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.gipu_android.databinding.InfolistActivityBinding
 import com.example.gipu_android.databinding.WritingActivityBinding
@@ -12,19 +17,113 @@ class WritingActivity: AppCompatActivity() {
     private val binding by lazy{
         WritingActivityBinding.inflate(layoutInflater)
     }
-    override fun onCreate(savedInstanceState: Bundle?){
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        lateinit var title: String
+        lateinit var content: String
 
-//        val db = Firebase.firestore
-//        val testData = hashMapOf(
-//            "name" to "yugyeong",
-//            "age" to "24",
-//            "country" to "korea"
-//        )
-//
-//        db.collection("게시물").add(testData)
+
+        binding.writingBack.setOnClickListener {
+            val intent = Intent(this, InfoListActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        with(binding) {
+            writingTitle.addTextChangedListener(object : TextWatcher {
+                var maxText = ""
+                override fun beforeTextChanged(pos: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                    maxText = pos.toString()
+                }
+
+                override fun onTextChanged(pos: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                    val toast = Toast.makeText(
+                        this@WritingActivity,
+                        "최대 18자까지 입력 가능합니다.",
+                        Toast.LENGTH_SHORT
+                    )
+                    if (writingTitle.length() > 18) {
+                        writingTitle.setText(maxText)
+                        writingTitle.setSelection(writingTitle.length())
+                        toast.show()
+                    } else {
+                        toast.cancel()
+                    }
+
+                    title = pos.toString()
+                }
+
+                override fun afterTextChanged(p0: Editable?) {
+                    // 텍스트 변경 후에 수행할 작업
+                }
+            })
+
+            writingContent.addTextChangedListener(object : TextWatcher {
+                var maxText = ""
+                override fun beforeTextChanged(pos: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                    maxText = pos.toString()
+                }
+
+                override fun onTextChanged(pos: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                    val toast = Toast.makeText(
+                        this@WritingActivity,
+                        "최대 1000자까지 입력 가능합니다.",
+                        Toast.LENGTH_SHORT
+                    )
+                    if (writingContent.length() > 1000) {
+                        writingContent.setText(maxText)
+                        writingContent.setSelection(writingTitle.length())
+                        toast.show()
+                    } else {
+                        toast.cancel()
+                    }
+                    content = pos.toString()
+                }
+
+                override fun afterTextChanged(p0: Editable?) {
+                    // 텍스트 변경 후에 수행할 작업
+                }
+            })
+
+        }
+
+        binding.writingFinish.setOnClickListener {
+            val db = Firebase.firestore
+            val testData = hashMapOf(
+                "title" to title,
+                "content" to content
+            )
+
+            db.collection("게시물").document("의진이 휴대폰").set(testData)
+
+            val intent = Intent(this, InfoListActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        binding.camera.setOnClickListener {
+            var popupMenu = PopupMenu(applicationContext, it)
+
+            menuInflater?.inflate(R.menu.camera_menu, popupMenu.menu)
+            popupMenu.show()
+            popupMenu.setOnMenuItemClickListener {
+                when (it.itemId) {
+                    R.id.test1 -> {
+                        Toast.makeText(applicationContext, "사진보관함", Toast.LENGTH_SHORT).show()
+                        return@setOnMenuItemClickListener true
+                    }
+                    R.id.test2 -> {
+                        Toast.makeText(applicationContext, "촬영", Toast.LENGTH_SHORT).show()
+                        return@setOnMenuItemClickListener true
+                    }else ->{
+                        return@setOnMenuItemClickListener false
+                    }
+                }
+            }
+        }
+
 
     }
 }
